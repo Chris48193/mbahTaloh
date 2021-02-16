427e0ef76c3312cbdb15eea25295a4578a03f11c
Chris48193 <yepmochristopher@yahoo.fr>
2021-02-16 01:53:20 +0100

    Deleting the source folder

--- source/html/sheet/test_mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-#echo (ini_get('max_upload_size'));
-    phpinfo(); //Affiche les informations du php actuelle dans un tableau.
-    #mail('yepmochristopher@yahoo.fr', 'test', 'blabla', "From: Thomas");
-?>
