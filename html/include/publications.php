3ca0a44aa28c4f0f2663833fd724e2f30e704341
Grace Kanouo <gntiwa@gmail.com>
2023-10-03 09:30:31 +0200

    Adding loader on publication uplaod

--- html/include/publications.php
+++ html/include/publications.php
@@ -32,6 +32,95 @@ catch (Exception $e)
         border-radius: 10px;
         text-align: justify;
     }
+
+        *,
+    ::befor,
+    ::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+
+    .loader-container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        background: whitesmoke;
+        width: 100%;
+        height: 100%;
+        margin: 100px auto 0;
+        display: none;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    .loader {
+        display: block;
+        position: relative;
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        border: 5px solid transparent;
+        border-top-color: crimson;
+        animation: spin 2s linear infinite;
+
+        
+    }
+
+
+    .loader::before,
+    .loader::after {
+        content: "";
+        position: absolute;
+        border-radius: 50%;
+        border: 5px solid transparent;
+    }
+
+
+    .loader::before {
+        top: 10px;
+        left: 10px;
+        right: 10px;
+        bottom: 10px;
+        border-top-color:rgb(252, 45, 86);
+        animation: spin 3s linear infinite;
+        
+    }
+
+
+    .loader::after {
+        top: 25px;
+        left: 25px;
+        right: 25px;
+        bottom: 25px;
+        border-top-color:rgb(192, 108, 125);
+        animation: spin 1.5s linear infinite;
+        
+    }
+
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg)
+        }
+        100%{
+        transform: rotate(360deg)
+        }
+    }
+
+
+
+    .overlay {
+        position: absolute;
+        display: none;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background:rgb(229, 210, 246);
+    }
 </style>
 
 
@@ -130,8 +219,9 @@ catch (Exception $e)
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card card-body bg-light">
-                            <!--<form id="publicationForm" method="POST" action="../php/save_publication.php">-->
-                            <form id="publicationForm" <?php echo "onsubmit='save_publication($id_membre)'"; ?>>
+                            <div id="liste_publication"></div>
+                          <!---  <form id="publicationForm" enctype="multipart/Form_data" method="POST" action="../php/save_publication.php">-->
+                            <form id="publicationForm" enctype="multipart/Form-data" <?php echo "onsubmit='save_publication($id_membre)'"; ?>>
                                 <div class="form-group">
                                     <label for="publication_body" class="control-label">Contenu de la publication</label>
                                     <textarea class="form-control" id="publication_body" name="publication_body" required="required" placeholder = "Que voulez vous dire, <?php echo $_SESSION['surname']; ?> ?"></textarea>
@@ -144,12 +234,19 @@ catch (Exception $e)
                                 <input type = "submit" class="btn btn-success btn-block" value = "Publier">
                             </form>
                         </div>
-                    </div>
+                    </div> 
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!----loader------>
+<div class="loader-container" id="loader">
+    <div class="overlay"></div>
+    <div class="loader"> </div>
+</div>
+
 
 <script>
     function toggle_publication_content(bouton_lire_plus, content_id, full_content){
@@ -184,12 +281,15 @@ catch (Exception $e)
                 preview_div.setAttribute('style', 'display:inline-block;');
                 document.getElementById('image_preview2').append(preview_div);
             })
+            
         }
     }
 
     // Fired when the publication form is submitted
     function save_publication(id_membre){
         event.preventDefault();
+        let loader = document.getElementById('loader');
+        loader.style.display = 'flex';
         input_publication_content = document.getElementById('publication_body');
         publication_content = input_publication_content.value;
         input_element = document.getElementById('publicationPicture');
@@ -199,20 +299,45 @@ catch (Exception $e)
         formDataPublicationContent.append('content', publication_content);
         formDataPublicationContent.append('id_membre', id_membre);
         formDataPublicationContent.append('action', 'save_publication_content');
+              
+        //Envoyer les données du formulair avec fetch
+
         fetch('../php/save_publication.php', {
             method: 'POST',
             body: formDataPublicationContent
             })
             .then((response) => {
+
+                if(response.ok){
+                    return response.text();//renvoie le text
+                }else {
+                    throw new Error('Reponse du serveur non ok');
+                }
+
                 console.log(response);
+                
+            })
+            .then((data) => {
+
+           
+                document.getElementById('liste_publication').innerHTML +=data;
+                publish_photo(pictures, 0, id_membre);
+                
             })
             .catch((error) => {
                 console.log(error);
+
+                 
             })
-        pictures.forEach(function(picture, i){
-            setTimeout(() => {
+        //pictures.forEach(function(picture, i){
+    
+    }
+
+    function publish_photo(pictures, i, id_membre){
+            if(i < pictures.length) {
+            
                 let formData = new FormData;
-                formData.append('pictures', picture);
+                formData.append('pictures', pictures[i]);
                 formData.append('id_membre', id_membre);
                 formData.append('action', 'save_publication_pictures');
                 fetch('../php/save_publication.php', {
@@ -220,16 +345,23 @@ catch (Exception $e)
                     body: formData
                     })
                     .then((response) => {
+                        
                         console.log(response);
+                       // console.log('set timeout for {i} picture');
+                        setTimeout(() => {publish_photo(pictures, i+1, id_membre)}, 1500 );
                     })
                     .catch((error) => {
                         console.log(error);
-                    })
-            }, 500 * i);
-        })
-        console.log("Reloading...")
-        // location.reload();
-    }
+                    });
+                    
+            }else{
+                loader.style.display = 'none';
+                console.log("Reloading...")
+                location.reload();
+                }
+
+        }
+            
 
     $(document).ready(function(){
         // Fancy box settings
@@ -243,6 +375,12 @@ catch (Exception $e)
                 $(this).addClass('transition');
             }, function(){
                 $(this).removeClass('transition');
+
+                
             });
+
+            setInterval(function() { 
+                save_publication(id_membre);}, 10000);
     });
+
 </script>
